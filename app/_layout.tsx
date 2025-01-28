353d96ce40ff64f091b91dfa87e7d5b041527962
itzsyboo <55495526+Fiona1121@users.noreply.github.com>
2025-01-28 11:17:40 -0800

    remove irrelvant filees and unused code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,13 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import "react-native-reanimated";
-
-import { useColorScheme } from "@/hooks/useColorScheme";
-import { Assets } from "react-native-ui-lib";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     WixMadeforText: require("../assets/fonts/WixMadeforText-VariableFont_wght.ttf"),
     Gaegu: require("../assets/fonts/Gaegu-Regular.ttf"),
@@ -34,12 +24,8 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
   );
 }
